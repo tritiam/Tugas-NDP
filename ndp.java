c6b2c7ed40e8f01102fe1180a2ab1828bed0bcde
Tritiam <61776791+tritiam@users.noreply.github.com>
2021-10-12 19:12:52 +0700

    Tugas basic java 1

---
+++ ndp.java
@@ -0,0 +1,39 @@
+public class ndp {
+    public static void main(String[] args) {
+    
+    // mendeklarasikan variabel
+    String first, last, alamat, hp, hobi;
+    int umur;
+    double berat, tinggi;
+    char goldar;
+    float jarak;
+    boolean online;
+
+    // mendeklarasikan nilai
+    first = "Tritia";
+    last = " Mutiara";
+    alamat = "Langkat";
+    hp = "081300000000";
+    hobi = "Baca webtoon";
+    umur = 19;
+    berat = 53.2;
+    tinggi = 158.22;
+    online = true;
+    jarak = 1.5f;
+    goldar = 'A';
+
+
+    // menampilkan output
+    System.out.println("Nama : " + first + last);
+    System.out.println("Alamat : " + alamat);
+    System.out.println("No hp : " + hp);
+    System.out.println("Hobi : " + hobi);
+    System.out.println("Umur : " + umur + " tahun");
+    System.out.println("Berat : " + berat + " kg");
+    System.out.println("Tinggi : " + tinggi + " cm");
+    System.out.println("Jarak : " + jarak + " km");
+    System.out.println("Golongan Darah : " + goldar);
+    System.out.println("Online : " + online);
+
+}
+}
